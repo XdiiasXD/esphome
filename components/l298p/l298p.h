b6a9cb74ee3585682190e2d9e1c07e860a129f9f
Diogo Dias <diasnoitesol@gmail.com>
2025-01-06 21:56:34 +0000

    changes

--- components/l298p/l298p.h
+++ components/l298p/l298p.h
@@ -6,21 +6,14 @@
 namespace esphome {
 namespace l298p {
 
-constexpr uint8_t MotorSpeedSet = 0x82;
-constexpr uint8_t DirectionSet = 0xaa;
-
-enum class MotorID : uint8_t { MOTOR1 = 1, MOTOR2 = 2 };
-enum class MotorDirection : uint8_t { ClockWise = 0x0a, AntiClockWise = 0x05 };
-
 class L298P : public i2c::I2CDevice, public Component {
  public:
   void setup() override;
   void loop() override;
-  void dump_config() override;
 
-  void set_motor_speed(MotorID motor_id, uint8_t speed);
-  void set_motor_direction(MotorDirection direction);
-  void stop_motor(MotorID motor_id);
+  void set_motor_speed(uint8_t motor_id, uint8_t speed);
+  void set_motor_direction(uint8_t direction);
+  void stop_motor(uint8_t motor_id);
 };
 
 }  // namespace l298p
