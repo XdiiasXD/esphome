24003074b0610c5e1d7b6f3ac9ae3dc49d247fad
Diogo Dias <diasnoitesol@gmail.com>
2025-01-06 21:42:36 +0000

    changed __init__.py

--- components/l298p/__init__.py
+++ components/l298p/__init__.py
@@ -3,15 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
 
-# Declare the namespace
-l298p_ns = cg.esphome_ns.namespace("l298p")
-L298P = l298p_ns.class_("L298P", cg.Component, i2c.I2CDevice)
+# Declare the namespace for the component
+l298p_ns = cg.esphome_ns.namespace('l298p')
+L298P = l298p_ns.class_('L298P', cg.Component, i2c.I2CDevice)
 
 # Define the configuration schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(L298P),
-    cv.Required("0x0f"): cv.i2c_address,  # Add I2C address validation
-}).extend(i2c.I2C_SCHEMA)
+}).extend(i2c.I2C_DEVICE_SCHEMA(0x0f))
 
 # Register the component
 async def to_code(config):
